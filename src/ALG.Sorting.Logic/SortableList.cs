57a4a71ff943b02b62b4dc2055d3666776be06ce
Arthur Heidt <arthur@arthurheidt.nl>
2020-09-21 21:27:01 +0200

    Impl. MergeSort

--- src/ALG.Sorting.Logic/SortableList.cs
+++ src/ALG.Sorting.Logic/SortableList.cs
@@ -1,5 +1,7 @@
 ﻿using ALG.Sorting.Logic.Abstractions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ALG.Sorting.Logic
 {
@@ -20,7 +22,90 @@ namespace ALG.Sorting.Logic
         /// <inheritdoc />
         public override void MergeSort()
         {
-            throw new NotImplementedException();
+            var list = new List<int>(this.Count);
+
+            for (var i = 0; i < this.Count; i++)
+            {
+                list.Add(this[i]);
+            }
+
+            var sorted = SortableList.MergeSort(list);
+
+            for (var i = 0; i < this.Count; i++)
+            {
+                this[i] = sorted[i];
+            }
+        }
+
+        private static List<int> MergeSort(List<int> list)
+            => list.Count switch
+            {
+                var x when x <= 1 => list,
+                _ => SortableList.MergeSortMoreThenOneElement(list),
+            };
+
+        private static List<int> MergeSortMoreThenOneElement(List<int> list)
+        {
+            var (left, right) = SortableList.DivideList(list);
+            var leftSorted = SortableList.MergeSort(left);
+            var rightSorted = SortableList.MergeSort(right);
+            return Merge(leftSorted, rightSorted);
+        }
+
+        private static (List<int> left, List<int> right) DivideList(List<int> list)
+        {
+            var capacityLeft = Convert.ToInt32(Math.Floor(list.Count / 2d));
+            var capacityRight = Convert.ToInt32(Math.Ceiling(list.Count / 2d));
+            var middleIndex = list.Count / 2;
+            var left = new List<int>(capacityLeft);
+            var right = new List<int>(capacityRight);
+
+            for (var i = 0; i < middleIndex; i++)
+            {
+                left.Add(list[i]);
+            }
+
+            for (var i = middleIndex; i < list.Count; i++)
+            {
+                right.Add(list[i]);
+            }
+
+            return (left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            var capacity = left.Count + right.Count;
+            var result = new List<int>(capacity);
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+
+            return result;
         }
     }
 }
